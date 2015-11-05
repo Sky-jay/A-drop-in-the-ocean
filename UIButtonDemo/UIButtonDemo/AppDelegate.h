e2830b5cf107849c552c673b9570b1fbf525bb99
Sky-jay <lizeshaogaofeng@163.com>
2015-11-05 22:14:52 +0800

    UIButtonDemo

---
+++ UIButtonDemo/UIButtonDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIButtonDemo
+//
+//  Created by qingyun on 15/11/5.
+//  Copyright (c) 2015年 Sky-jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIButtonDemo/UIButtonDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIButtonDemo
+//
+//  Created by qingyun on 15/11/5.
+//  Copyright (c) 2015年 Sky-jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
