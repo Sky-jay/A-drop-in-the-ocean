9cd4e6ee310fe8cd2af2f86849b1ec59644adb96
iGhibli <iGhibli@163.com>
2015-12-04 22:30:38 +0800

    Add some comments

--- TextViewDemo/TextViewDemo/ViewController.m
+++ TextViewDemo/TextViewDemo/ViewController.m
@@ -31,29 +31,30 @@
 
 - (void)configTextView
 {
-    //选择文字设置，黑体
+    //选择文字，设置黑体
     NSRange boldRange = [_textView.text rangeOfString:@"假如"];
-    //
+    //选择文字，设置带背景颜色
     NSRange bgRang = [_textView.text rangeOfString:@"生活"];
-    //
+    //选择文字，设置带下划线
     NSRange underLineRang = [_textView.text rangeOfString:@"心儿"];
-    //
+    //选择文字，设置字体颜色
     NSRange tintedRange = [_textView.text rangeOfString:@"过去"];
     
-    //
+    //创建一个属性文本
     NSMutableAttributedString *attributedStr = [[NSMutableAttributedString alloc]initWithAttributedString:_textView.attributedText];
-    //
+    //添加属性
     [attributedStr addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:20] range:boldRange];
     [attributedStr addAttribute:NSBackgroundColorAttributeName value:[UIColor redColor] range:bgRang];
     [attributedStr addAttribute:NSUnderlineStyleAttributeName value:@1 range:underLineRang];
     [attributedStr addAttribute:NSForegroundColorAttributeName value:[UIColor greenColor] range:tintedRange];
-    //
+    //添加图片
+    //文本附着图片
     NSTextAttachment *attachment = [[NSTextAttachment alloc]init];
     attachment.image = [UIImage imageNamed:@"假如生活欺骗了你.jpg"];
     NSAttributedString *attachStr = [NSAttributedString attributedStringWithAttachment:attachment];
-    //
+    //把图片属性字符串追加到attributedStr后面
     [attributedStr appendAttributedString:attachStr];
-    
+    //将属性文本赋值给TextView的属性文本
     _textView.attributedText = attributedStr;
     
 }
