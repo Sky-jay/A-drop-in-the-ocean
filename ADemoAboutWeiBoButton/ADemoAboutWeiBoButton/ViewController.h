5c0e963256e0f18b15851fe949febefec56ffc4f
Sky-jay <lizeshaogaofeng@163.com>
2015-12-07 21:33:01 +0800

    Add a demo about weibo button.

---
+++ ADemoAboutWeiBoButton/ADemoAboutWeiBoButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADemoAboutWeiBoButton
+//
+//  Created by qingyun on 15/12/7.
+//  Copyright (c) 2015年 Sky-jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ADemoAboutWeiBoButton/ADemoAboutWeiBoButton/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ADemoAboutWeiBoButton
+//
+//  Created by qingyun on 15/12/7.
+//  Copyright (c) 2015年 Sky-jay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
