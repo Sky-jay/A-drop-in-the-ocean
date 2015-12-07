b4b934ce3527f75e647a7f886d5123aed9a30861
Sky-jay <lizeshaogaofeng@163.com>
2015-12-07 21:39:38 +0800

    Create buttons.

--- ADemoAboutWeiBoButton/ADemoAboutWeiBoButton/ViewController.m
+++ ADemoAboutWeiBoButton/ADemoAboutWeiBoButton/ViewController.m
@@ -8,20 +8,52 @@
 
 #import "ViewController.h"
 
+#define kScreenW [UIScreen mainScreen].bounds.size.width
+#define kScreenH [UIScreen mainScreen].bounds.size.height
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
+static BOOL isShow = NO;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self createBtns];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)btnAction:(UIButton *)sender {
+}
+
+- (void)createBtns
+{
+    NSArray *btnImages = @[@"tabbar_compose_camera", @"tabbar_compose_idea", @"tabbar_compose_lbs", @"tabbar_compose_more", @"tabbar_compose_photo", @"tabbar_compose_review"];
+    CGFloat btnW = 71.f;
+    CGFloat btnH = 71.f;
+    CGFloat XMargin = (kScreenW - 3 * btnW) / 4;
+    CGFloat YMargin = 32;
+    for (int i = 0; i < 6; i++) {
+        int row = i / 3;
+        int column = i % 3;
+        CGFloat btnX = XMargin + (btnW + XMargin) * column;
+        CGFloat btnY = YMargin + (btnH + YMargin) * row + kScreenH;
+        UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(btnX, btnY, btnW, btnH)];
+        UIImage *image = [UIImage imageNamed:btnImages[i]];
+        [btn setBackgroundImage:image forState:UIControlStateNormal];
+        [self.view addSubview:btn];
+    }
+}
+
+- (void)showBtnsWithAnimation:(BOOL)isOrNo
+{
+    
+}
+
+- (void)hiddenBtnsWithAnimation:(BOOL)isOrNo
+{
+    
 }
 
 @end
